c62200c6bd4e4010af0767a17785ff6eb5f318a6
SaveApis | Felix Hake <felix.hake@saveapis.com>
2025-02-05 19:13:22 +0100

    chore: Create CI pipeline
    Author: Felix Hake

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
+![Super-Linter](https://github.com/SaveApis/Inventarisierung/actions/workflows/lint.yml/badge.svg)
+![CI](https://github.com/SaveApis/Inventarisierung/actions/workflows/ci.yml/badge.svg)
+
 # Inventarisierung
---
+++ tests/Inventarisierung.Tests/ExampleTest.cs
@@ -0,0 +1,14 @@
+﻿namespace Inventarisierung.Tests;
+
+public class ExampleTest
+{
+    [Test]
+    public async Task Test1()
+    {
+        // Arrange
+        var value = "test";
+
+        // Assert
+        await Assert.That(value).IsEqualTo("test");
+    }
+}
