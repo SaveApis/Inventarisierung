162855c09f4e3470697e96a969901404b4614576
SaveApis | Felix Hake <felix.hake@saveapis.com>
2025-02-06 20:34:20 +0100

    chore: Create CD pipeline
    Author: Felix Hake

--- tests/Inventarisierung.Tests/ExampleTest.cs
+++ tests/Inventarisierung.Tests/ExampleTest.cs
@@ -6,9 +6,10 @@ public class ExampleTest
     public async Task Test1()
     {
         // Arrange
-        var value = "test";
+        var number = Random.Shared.Next(1, 100);
+        var value = $"test-{number}";
 
         // Assert
-        await Assert.That(value).IsEqualTo("test");
+        await Assert.That(value).IsEqualTo($"test-{number}");
     }
 }
